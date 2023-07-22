9f11af5c188848a3f91c37b6a9f90d05fba73730
Vijay <axelblaze14022000@gmail.com>
2023-07-22 18:06:22 +0530

    made the navbar transparent based scrolling effect

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import { styles } from '../styles'
@@ -8,13 +8,29 @@ import { logo, menu, close } from '../assets'
 const Navbar = () => {
   const [active, setActive] = useState("")
   const [toggle, setToggle] = useState(false)
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      if (scrollTop > 100) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-    <nav className={`${styles.paddingX} w-full flex items-center py-2 fixed top-0 z-20 bg-primary`}>
+    <nav className={`${styles.paddingX} w-full flex items-center py-2 fixed top-0 z-20 ${scrolled ? 'bg-primary' : 'bg-transparent'}`}>
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link to="/" className='flex items-center gap-2' onClick={() => { setActive(""); window.scrollTo(0, 0); }}>
           <img src={logo} alt="logo" className='w-9 h-9 sm:w-16 sm:h-16 object-contain' />
-          <p className='text-white text-[18px] font-bold cursor-pointer flex'>VKS&nbsp;<span className='sm:block hidden'>| Portfolio</span></p>
+          <p className='text-white text-[18px] font-bold cursor-pointer flex'>VKS | Portfolio</p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
           {navLinks.map((link) => (
